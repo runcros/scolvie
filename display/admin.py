2c7910085a3d2f7ce717b71ffd725ff4504a990f
runcros <andrelauret@icloud.com>
2022-11-06 16:56:56 +0400

    Register new objects

--- display/admin.py
+++ display/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import AnnouncementType, Announcement
+from .models import AnnouncementType, Announcement, Teacher, Room, Slot, Planning
 
 admin.site.register(AnnouncementType)
 admin.site.register(Announcement)
+admin.site.register(Teacher)
+admin.site.register(Room)
+admin.site.register(Slot)
+admin.site.register(Planning)
